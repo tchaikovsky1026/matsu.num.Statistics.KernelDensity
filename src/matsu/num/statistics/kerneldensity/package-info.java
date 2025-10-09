5243856133cf503043f910f88f5278e28688fef2
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-10-09 13:45:33 +0900

    add: 1DKDEのパッケージの作成

---
+++ src/matsu/num/statistics/kerneldensity/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * カーネル密度推定を扱うルートパッケージ.
+ */
+package matsu.num.statistics.kerneldensity;
