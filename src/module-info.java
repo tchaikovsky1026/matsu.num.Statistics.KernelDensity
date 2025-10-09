148be044d547778dc0fe63d5e7219c92966443f4
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-10-09 13:14:22 +0900

    add: module-info.java

---
+++ src/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * カーネル密度推定を扱うモジュール.
+ * 
+ * <p>
+ * <i>依存モジュール:</i> <br>
+ * (無し)
+ * </p>
+ * 
+ * @author Matsuura Y.
+ * @version 0.1.0
+ */
+module matsu.num.Statistics.KernelDensity {
+
+}
