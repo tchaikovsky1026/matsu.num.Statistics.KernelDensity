68c091ba50e829c5b2626b8d26eadb9e3736c3fb
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-16 08:25:38 +0900

    test: Power2DftInjectedCyclicConvolutionにテストケースを追加
    - calcAcceptableSizeメソッドのテストを追加
    - テストされるPower2Dftを追加
    - double値のassertionの誤差範囲を変更

--- test/matsu/num/statistics/kerneldensity/convol/incubator/Power2DftInjectedCyclicConvolutionTest.java
+++ test/matsu/num/statistics/kerneldensity/convol/incubator/Power2DftInjectedCyclicConvolutionTest.java
@@ -7,9 +7,12 @@
 
 package matsu.num.statistics.kerneldensity.convol.incubator;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Arrays;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
@@ -28,39 +31,95 @@ import matsu.num.statistics.kerneldensity.EffectiveCyclicConvolutionStubForTesti
 @RunWith(Enclosed.class)
 final class Power2DftInjectedCyclicConvolutionTest {
 
-    private static final EffectiveCyclicConvolution TESTING_CONV =
-            new Power2DftInjectedCyclicConvolution(new NaivePower2DftForTesting());
-
     private static final EffectiveCyclicConvolution REFERENCE_CONV =
             new EffectiveCyclicConvolutionStubForTesting();
 
     @RunWith(Theories.class)
-    public static class ランダムな信号でテスト {
+    public static class 受け入れ可能サイズに関するテスト {
+
+        private final EffectiveCyclicConvolution convolution =
+                new Power2DftInjectedCyclicConvolution(new NaivePower2DftForTesting());
 
         @DataPoints
-        public static int[] sizes = { 1, 2, 4, 8, 16, 32, 512 };
+        public static int[][] argAndExpectedSet = {
+                { -1, 1 },
+                { 0, 1 },
+                { 1, 1 },
+                { 2, 2 },
+                { 3, 4 },
+                { 4, 4 },
+                { 5, 8 }
+        };
 
         @Theory
-        public void test_サイズ8(int size) {
+        public void test_calcAcceptableSizeメソッドの引数戻り値ペアのテスト(int[] argAndExpected) {
+            int arg = argAndExpected[0];
+            int expected = argAndExpected[1];
+            assertThat(convolution.calcAcceptableSize(arg), is(expected));
+        }
+    }
+
+    @RunWith(Theories.class)
+    public static class ランダムな信号でテスト_DFTバリエーション {
+
+        /**
+         * テストに使うDFTのセット.
+         */
+        @DataPoints
+        public static Set<Power2Dft> testingDfts = testingDfts();
+
+        /**
+         * テストするサイズ.
+         */
+        @DataPoints
+        public static int[] sizes = { 1, 2, 4, 8, 16, 32, 512, 2048 };
+
+        @Theory
+        public void test_与えたサイズとDFTを用いて畳み込みをテストする(int size, Power2Dft dft) {
+            EffectiveCyclicConvolution testingConv =
+                    new Power2DftInjectedCyclicConvolution(dft);
+
             final int iteration = 10;
 
             for (int c = 0; c < iteration; c++) {
                 double[] f = generateRandomSignal(size);
                 double[] g = generateRandomSignal(size);
 
-                double[] result = TESTING_CONV.apply(f, g);
+                double[] result = testingConv.apply(f, g);
                 double[] expected = REFERENCE_CONV.apply(f, g);
 
+                double scale = Arrays.stream(expected)
+                        .map(Math::abs)
+                        .max().orElse(1E-200);
+
                 for (int j = 0; j < size; j++) {
-                    assertThat(result[j], is(closeTo(expected[j], 1E-12)));
+                    assertThat(
+                            result[j], is(
+                                    closeTo(
+                                            expected[j],
+                                            scale * 1E-15 * Math.min(size, 100))));
                 }
             }
         }
     }
 
+    /**
+     * 与えた長さのランダムな信号を生成する. <br>
+     * 信号の値は -0.5 以上 0.5 未満である
+     * (境界値は怪しいかも).
+     */
     private static double[] generateRandomSignal(int length) {
         return IntStream.range(0, length)
                 .mapToDouble(i -> (ThreadLocalRandom.current().nextDouble() - 0.5))
                 .toArray();
+    }
+
+    /**
+     * このクラスでテストに用いられる {@link Power2Dft} のセットを返す.
+     */
+    private static Set<Power2Dft> testingDfts() {
+        return Set.of(
+                new NaivePower2DftForTesting(),
+                new Power2Fft());
     }
 }
