4746d6552c577a8e1f45aaa0ed1217d12e2f20df
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-15 18:22:25 +0900

    feat: Power2DftにmaxAcceptableSize()を追加,契約の明確化

--- src/matsu/num/statistics/kerneldensity/convol/incubator/Power2Dft.java
+++ src/matsu/num/statistics/kerneldensity/convol/incubator/Power2Dft.java
@@ -6,12 +6,12 @@
  */
 
 /*
- * 2025.12.14
+ * 2025.12.15
  */
 package matsu.num.statistics.kerneldensity.convol.incubator;
 
 /**
- * 2の累乗の信号サイズを受け付ける DFT (離散 Fourier 変換) 機能. <br>
+ * 2の累乗のシグナルサイズを受け付ける DFT (離散 Fourier 変換) 機能. <br>
  * 逆変換も同時に扱う.
  * 
  * <p>
@@ -25,9 +25,37 @@ package matsu.num.statistics.kerneldensity.convol.incubator;
  * 変換 {@literal ->} 逆変換により N 倍になる.
  * </p>
  * 
+ * <p>
+ * 読み込むことができるシグナルサイズは具象にゆだねられる. <br>
+ * その値は, 抽象メソッド ({@link #maxAcceptableSize()}) により取得可能である. <br>
+ * 共通に, 2<sup>25</sup>までは必ず対応している.
+ * </p>
+ * 
  * @author Matsuura Y.
  */
 interface Power2Dft {
+
+    /**
+     * このインスタンスの {@link #dft(double[][])}, {@link #idft(double[][])}
+     * メソッドが対応できるシグナルサイズの最大値を返す. <br>
+     * 必ず 2<sup>25</sup> 以上の2の累乗数である.
+     * 
+     * <p>
+     * このメソッドの戻り値に一致するサイズを持つシグナルは,
+     * 必ず {@link #dft(double[][])}, {@link #idft(double[][])}
+     * の引数になることができる.
+     * </p>
+     * 
+     * @implSpec
+     *               メソッド説明にしたがって実装すること.
+     * 
+     *               <p>
+     *               この戻り値は, インスタンスのライフサイクル全体で不変でなければならない.
+     *               </p>
+     * 
+     * @return シグナルの最大サイズ
+     */
+    public abstract int maxAcceptableSize();
 
     /**
      * シグナルを離散 Fourier 変換する.
@@ -36,12 +64,11 @@ interface Power2Dft {
      * 入力シグナルは {@code double[2][size]} であり,
      * {@code signal[0]} が実部を,
      * {@code signal[1]} が虚部を表す. <br>
-     * {@code size} は2の累乗でなければならない.
+     * {@code size} は (1以上の) 2の累乗でなければならない.
      * </p>
      * 
      * <p>
-     * シグナルの長さは, 2<sup>25</sup>までは必ず対応している. <br>
-     * (これ以上の長さが与えられても, 直ちに例外をスローするわけではない.)
+     * 対応可能なシグナルの長さの最大値は, {@link #maxAcceptableSize()} の戻り値で規定される.
      * </p>
      * 
      * <p>
@@ -52,10 +79,10 @@ interface Power2Dft {
      * 
      * @param signal シグナル
      * @return 離散 Fourier 変換の結果
-     * @throws IllegalArgumentException シグナルが正しい形式でない場合
+     * @throws IllegalArgumentException シグナルが正しい形式でない場合 (シグナルサイズが大きすぎる場合を含む)
      * @throws NullPointerException 引数にnullが含まれる場合
      */
-    double[][] dft(double[][] signal);
+    public abstract double[][] dft(double[][] signal);
 
     /**
      * シグナルを逆離散 Fourier 変換する.
@@ -64,12 +91,11 @@ interface Power2Dft {
      * 入力シグナルは {@code double[2][size]} であり,
      * {@code signal[0]} が実部を,
      * {@code signal[1]} が虚部を表す. <br>
-     * {@code size} は2の累乗でなければならない.
+     * {@code size} は (1以上の) 2の累乗でなければならない.
      * </p>
      * 
      * <p>
-     * シグナルの長さは, 2<sup>25</sup>までは必ず対応している. <br>
-     * (これ以上の長さが与えられても, 直ちに例外をスローするわけではない.)
+     * 対応可能なシグナルの長さの最大値は, {@link #maxAcceptableSize()} の戻り値で規定される.
      * </p>
      * 
      * <p>
@@ -80,8 +106,8 @@ interface Power2Dft {
      * 
      * @param signal シグナル
      * @return 離散 Fourier 変換の結果
-     * @throws IllegalArgumentException シグナルが正しい形式でない場合
+     * @throws IllegalArgumentException シグナルが正しい形式でない場合 (シグナルサイズが大きすぎる場合を含む)
      * @throws NullPointerException 引数にnullが含まれる場合
      */
-    double[][] idft(double[][] signal);
+    public abstract double[][] idft(double[][] signal);
 }
