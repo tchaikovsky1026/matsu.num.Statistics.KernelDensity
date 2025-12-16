ff8b7946c560965b97a239c06adcc41b1b6e4206
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-12-16 10:32:56 +0900

    docs: update package-info

--- src/matsu/num/statistics/kerneldensity/conv/package-info.java
+++ src/matsu/num/statistics/kerneldensity/conv/package-info.java
@@ -7,5 +7,12 @@
 
 /**
  * 巡回畳み込みの実装提供のためのパッケージ.
+ * 
+ * <p>
+ * このパッケージでは,
+ * {@link matsu.num.statistics.kerneldensity.EffectiveCyclicConvolution}
+ * の具象実装を提供する. <br>
+ * 詳細は各具象クラスを参照のこと.
+ * </p>
  */
 package matsu.num.statistics.kerneldensity.conv;
